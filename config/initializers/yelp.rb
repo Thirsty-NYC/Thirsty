6813a845d944c3a0854cb7a2a425d81542795db8
Iris Martinez <i.martinez8@umiami.edu>
2014-11-24 13:13:14 -0500

    kjafs;kh

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,5 +1,8 @@
-client = Yelp::Client.new({ consumer_key: ENV['YELP_CONSUMER_KEY'],
-                            consumer_secret: ENV['YELP_CONSUMER_SECRET'],
-                            token: ENV['YELP_TOKEN'],
-                            token_secret: ENV['YELP_TOKEN_SECRET']
-                          })
+require 'yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = ENV["YELP_CONSUMER_KEY"]
+  config.consumer_secret = ENV["YELP_CONSUMER_SECRET"]
+  config.token = ENV["YELP_TOKEN"]
+  config.token_secret = ENV["YELP_TOKEN_SECRET"]
+end
