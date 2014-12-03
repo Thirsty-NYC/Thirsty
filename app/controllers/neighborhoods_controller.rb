5e5a5313c00b2ac06651e0cc9729f68e457013ca
Iris Martinez <i.martinez8@umiami.edu>
2014-12-02 20:35:49 -0500

    done for the day

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,14 +1,25 @@
 class NeighborhoodsController < ApplicationController
 	def index 
-
-
-	end 
-
-
+	end
 	def show
-		binding.pry
 		neighborhood = Neighborhood.find_by(params["id"]).name
 		bars = Bar.where(neighborhood: neighborhood)
-		render :json => bars
+		array = []
+		
+		bars.each do |bar|
+			specials = Special.where(bar_id: bar.id)
+			specials.each do |special|
+				hash = {}
+				hash[special.day] = [special.info, special.time]
+				array << hash
+			end 
+		end 
+
+		array2 = []
+		array2 << bars
+		array2 << array
+
+		render :json => array2
+
 	end 
 end 
